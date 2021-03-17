44da3c9f4424eae549c1f6f108030737195d56a3
Mahfudin98 <mahfudinnawawi@gmail.com>
2021-03-17 23:42:41 +0700

    update pixel fb js

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -53,7 +53,12 @@
             startTimer(fiveMinutes, display);
         };
     </script>
-
+    <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '{{ $links->pixel }}');fbq('track', '{{ $links->type_pixel }}');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id={{ $links->pixel }}&ev={{ $links->type_pixel }}&noscript=1"/>
+    </noscript>
     <style>
         .tengah {
           padding: 100px 0;
